68145c217ab63ac983c3f167fb0780fe09d49b95
mohammadreza salek <rezasalek75@gmail.com>
2025-04-07 13:32:20 +0330

    improve code is also add

--- train-3.js
+++ train-3.js
@@ -1,15 +1,44 @@
+// function collatz(n) {
+//   let result = [];
+//   result.push(n);
+//   while (n !== 1) {
+//     if (n % 2 === 0) {
+//       result.push(n / 2);
+//       n = n / 2;
+//     } else {
+//       result.push(3 * n + 1);
+//       n = 3 * n + 1;
+//     }
+//   }
+//   return result.join(" / ").replaceAll(" / ", "->");
+// }
+// collatz(3);
+// Improve
+
 function collatz(n) {
   let result = [];
-  result.push(n);
-  while (n !== 1) {
-    if (n % 2 === 0) {
-      result.push(n / 2);
-      n = n / 2;
-    } else {
-      result.push(3 * n + 1);
-      n = 3 * n + 1;
+  const recursive = (n) => {
+    // if (n !== 1) {
+    //   if (n % 2 === 0) {
+    //     result.push(n);
+    //     return recursive(n / 2);
+    //   } else {
+    //     result.push(n);
+    //     return recursive(3 * n + 1);
+    //   }
+    // } else {
+    //   result.push(n);
+    // }
+    // Improve
+    result.push(n);
+    if (n !== 1) {
+      return recursive(n % 2 === 0 ? n / 2 : 3 * n + 1);
     }
-  }
-  return result.join(" / ").replaceAll(" / ", "->");
+  };
+  recursive(n);
+  //   return result.join(" / ").replaceAll(" / ", "->");
+  // Improve
+  return result.join("->");
 }
+
 collatz(3);
