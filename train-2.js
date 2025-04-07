1f0656a76521aaecc1932b4e95c95724660b974f
mohammadreza salek <rezasalek75@gmail.com>
2025-04-07 11:45:14 +0330

    another version of solution

--- train-2.js
+++ train-2.js
@@ -1,20 +1,31 @@
+// function firstDup(s) {
+//   let strtoArr = s.split("");
+//   let repeatItem = [];
+//   let ind = [];
+//   for (let i = 0; i < strtoArr.length; i++) {
+//     for (let j = i + 1; j < strtoArr.length; j++) {
+//       if (strtoArr[i] === strtoArr[j]) {
+//         repeatItem.push(strtoArr[i]);
+//       }
+//     }
+//   }
+//   for (let k = 0; k < repeatItem.length; k++) {
+//     ind.push(strtoArr.indexOf(repeatItem[k]));
+//   }
+//   let sortArr = ind.sort((a, b) => a - b);
+//   return strtoArr[sortArr[0]];
+// }
+
+// Improvment
+
 function firstDup(s) {
-  let strtoArr = s.split("");
-  let repeatItem = [];
-  let ind = [];
-  for (let i = 0; i < strtoArr.length; i++) {
-    for (let j = i + 1; j < strtoArr.length; j++) {
-      if (strtoArr[i] === strtoArr[j]) {
-        repeatItem.push(strtoArr[i]);
-      }
-    }
+  const seen = new Set();
+  for (let char of s) {
+    if (seen.has(char)) return char;
+    seen.add(char);
   }
-  for (let k = 0; k < repeatItem.length; k++) {
-    ind.push(strtoArr.indexOf(repeatItem[k]));
-  }
-  let sortArr = ind.sort((a, b) => a - b);
-  return strtoArr[sortArr[0]];
+  return undefined;
 }
 
-let str = "bar";
+let str = "translator";
 firstDup(str);
