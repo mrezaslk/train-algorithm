0aab84531bc6bdc6b71ac9770906b4b08d2571f5
mohammadreza salek <rezasalek75@gmail.com>
2025-04-07 13:15:48 +0330

    collatz done

---
+++ train-3.js
@@ -0,0 +1,15 @@
+function collatz(n) {
+  let result = [];
+  result.push(n);
+  while (n !== 1) {
+    if (n % 2 === 0) {
+      result.push(n / 2);
+      n = n / 2;
+    } else {
+      result.push(3 * n + 1);
+      n = 3 * n + 1;
+    }
+  }
+  return result.join(" / ").replaceAll(" / ", "->");
+}
+collatz(3);
