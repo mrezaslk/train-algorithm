5bb1562311a2ca62e530f91f1fc0bb8f8fc387c7
mohammadreza salek <rezasalek75@gmail.com>
2025-05-12 11:26:25 +0330

    findEvenNumbers

---
+++ leetcode/findEvenNumbers.js
@@ -0,0 +1,23 @@
+var findEvenNumbers = function (digits) {
+  //   let result = new Set();
+  //   for (let i = 0; i < digits.length; i++) {
+  //     for (let j = 0; j < digits.length; j++) {
+  //       for (let k = 0; k < digits.length; k++) {
+  //         if (i !== j && j !== k && i !== k) {
+  //           if (digits[i] > 0) {
+  //             let numq = digits[i] * 100 + digits[j] * 10 + digits[k];
+  //             if (!Boolean(numq % 2)) {
+  //               result.add(numq);
+  //             }
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  //   return [...result].sort((a, b) => a - b);
+  //   Improvment
+  
+};
+
+let digits = [1, 2, 3];
+findEvenNumbers(digits);
