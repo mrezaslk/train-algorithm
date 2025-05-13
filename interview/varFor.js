c22b96791fece43e30ac6a19395aff80e5647120
mohammadreza salek <rezasalek75@gmail.com>
2025-05-13 15:15:11 +0330

    {interview question}

---
+++ interview/varFor.js
@@ -0,0 +1,13 @@
+// for (var i = 0; i < 3; i++) {
+//   setTimeout(() => {
+//     console.log(i);
+//   }, 100);
+// }
+
+// for (var i = 0; i < 3; i++) {
+//   ((j) => {
+//     setTimeout(() => {
+//       console.log(j);
+//     }, 100);
+//   })(i);
+// }
