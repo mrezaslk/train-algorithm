57c6957c20fbd4482410b2154180b3f86e2f9a90
mohammadreza salek <rezasalek75@gmail.com>
2025-05-14 11:46:38 +0330

    roman improvment

--- leetcode/romanToInt.js
+++ leetcode/romanToInt.js
@@ -2,6 +2,44 @@
  * @param {string} s
  * @return {number}
  */
+// var romanToInt = function (s) {
+//   const romanObj = {
+//     I: 1,
+//     V: 5,
+//     X: 10,
+//     L: 50,
+//     C: 100,
+//     D: 500,
+//     M: 1000,
+//   };
+//   const conditionRomanObj = {
+//     IV: 4,
+//     IX: 9,
+//     XL: 40,
+//     XC: 90,
+//     CD: 400,
+//     CM: 900,
+//   };
+//   let totalNum = 0;
+//   let workingStr = s;
+//   for (const [key, value] of Object.entries(conditionRomanObj)) {
+//     if (s.includes(key)) {
+//       totalNum += value;
+//       workingStr = workingStr.replace(key, "");
+//     }
+//   }
+//   let numToArray = workingStr.split("");
+//   for (let i = 0; i < numToArray.length; i++) {
+//     const element = romanObj[numToArray[i]];
+//     totalNum += element;
+//   }
+//   return totalNum;
+// };
+// let s = "MMCMLXIV";
+// romanToInt(s);
+
+// Improvement
+
 var romanToInt = function (s) {
   const romanObj = {
     I: 1,
@@ -12,28 +50,18 @@ var romanToInt = function (s) {
     D: 500,
     M: 1000,
   };
-  const conditionRomanObj = {
-    IV: 4,
-    IX: 9,
-    XL: 40,
-    XC: 90,
-    CD: 400,
-    CM: 900,
-  };
   let totalNum = 0;
-  let workingStr = s;
-  for (const [key, value] of Object.entries(conditionRomanObj)) {
-    if (s.includes(key)) {
-      totalNum += value;
-      workingStr = workingStr.replace(key, "");
+  for (let i = 0; i < s.length; i++) {
+    const current = romanObj[s[i]];
+    const next = romanObj[s[i + 1]];
+    if (next && current < next) {
+      totalNum -= current;
+    } else {
+      totalNum += current;
     }
-  }
-  let numToArray = workingStr.split("");
-  for (let i = 0; i < numToArray.length; i++) {
-    const element = romanObj[numToArray[i]];
-    totalNum += element;
   }
   return totalNum;
 };
-let s = "MMCMLXIV";
+
+let s = "MCMXCIV";
 romanToInt(s);
